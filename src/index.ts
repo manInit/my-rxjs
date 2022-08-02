616937d876483ea8787c60966b079403983220ca
manInit <info.denisbronnikov@gmail.com>
2022-08-02 18:42:23 +0900

    first usage example

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-import Observable from './core/observable';
-let a = 5
-console.log(a)
+import Observable from './core/observable'
+import Observer from './core/observer'
+
+const a$ = new Observable((observer: Observer) => {
+  observer.next(2)
+  observer.next(3)
+  observer.next(4)
+  observer.next(5)
+})
+
+a$.subscribe(x => {
+  console.log(x)
+})
