ca7d6813efb536d67cc62902d06916cd53aab048
manInit <info.denisbronnikov@gmail.com>
2022-08-03 17:21:33 +0900

    example for pipe

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
-import fromEvent from './operators/creation/fromEvent'
+import { interval, map, even, multiply, take, range, fromEvent } from './operators/'
 
 const a$ = fromEvent(document, 'click')
 
-a$.subscribe(x => {
-  console.log(x)
+a$.pipe(
+  map(e => e.clientX),
+  even,
+  take(5),
+).subscribe(val => {
+  console.log(val)
 })
